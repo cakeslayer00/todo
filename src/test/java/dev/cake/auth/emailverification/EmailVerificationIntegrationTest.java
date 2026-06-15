409bb46aed527b854be71cb6e92cd2e2106e5e24
cakeslayer00 <mainacc1838@gmail.com>
2026-06-15 17:35:21 +0500

    chore: add more tests regarding email verification

--- src/test/java/dev/cake/auth/emailverification/EmailVerificationIntegrationTest.java
+++ src/test/java/dev/cake/auth/emailverification/EmailVerificationIntegrationTest.java
@@ -1,6 +1,7 @@
 package dev.cake.auth.emailverification;
 
 import dev.cake.auth.common.AbstractIntegrationTest;
+import dev.cake.auth.common.exception.InvalidTokenException;
 import dev.cake.auth.emailverification.event.EmailVerificationRequestedEvent;
 import dev.cake.auth.identity.User;
 import dev.cake.auth.identity.UserRepository;
@@ -9,6 +10,7 @@ import dev.cake.auth.registration.event.UserRegisteredEvent;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -27,8 +29,10 @@ import org.springframework.transaction.support.TransactionTemplate;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Transactional
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -57,12 +61,35 @@ public class EmailVerificationIntegrationTest extends AbstractIntegrationTest {
     TransactionTemplate txTemplate;
 
     @Test
+    void when_invalid_token_received_throw_exception() {
+        assertThatThrownBy(() -> emailVerificationService.verify("randomstring")).isInstanceOf(InvalidTokenException.class);
+    }
+
+    @Test
+    void when_expired_token_received_throw_exception() {
+        var user = generatePersistedUser();
+
+        var verification = issueVerificationTokenAndGetVerification(user);
+        verification.emailVerification().setExpiresAt(Instant.now());
+
+        assertThatThrownBy(() -> emailVerificationService.verify(verification.rawToken))
+                .isInstanceOf(InvalidTokenException.class);
+    }
+
+    @Test
+    void when_consumed_token_received_throw_exception() {
+        var user = generatePersistedUser();
+
+        var verification = issueVerificationTokenAndGetVerification(user);
+        verification.emailVerification().setConsumedAt(Instant.now());
+
+        assertThatThrownBy(() -> emailVerificationService.verify(verification.rawToken))
+                .isInstanceOf(InvalidTokenException.class);
+    }
+
+    @Test
     void when_valid_token_received_email_verified() {
-        var user = userRepository.save(User.builder()
-                .username("johndoe")
-                .email("johndoe@example.com")
-                .emailVerified(false)
-                .build());
+        var user = generatePersistedUser();
 
         var token = issueVerificationToken(user);
         emailVerificationService.verify(token);
@@ -77,11 +104,7 @@ public class EmailVerificationIntegrationTest extends AbstractIntegrationTest {
 
     @Test
     void when_user_registered_event_published_email_verification_event_emitted() {
-        var user = userRepository.saveAndFlush(User.builder()
-                .username("johndoe")
-                .email("johndoe@example.com")
-                .emailVerified(false)
-                .build());
+        var user = generatePersistedUser();
 
         applicationEventPublisher.publishEvent(new UserRegisteredEvent(user.getPublicId()));
 
@@ -97,14 +120,9 @@ public class EmailVerificationIntegrationTest extends AbstractIntegrationTest {
         try (Consumer<String, EmailVerificationRequestedEvent> consumer = createTestConsumer()) {
             embeddedKafkaBroker.consumeFromAnEmbeddedTopic(consumer, kafkaTopicProperties.emailVerificationRequested().name());
 
-            var user = userRepository.saveAndFlush(User.builder()
-                    .username("johndoe")
-                    .email("johndoe@example.com")
-                    .emailVerified(false)
-                    .build());
+            var user = generatePersistedUser();
 
             txTemplate.executeWithoutResult(_ -> {
-                userRepository.save(user);
                 applicationEventPublisher.publishEvent(new UserRegisteredEvent(user.getPublicId()));
             });
 
@@ -121,6 +139,34 @@ public class EmailVerificationIntegrationTest extends AbstractIntegrationTest {
         }
     }
 
+    @Test
+    void when_resend_invoked_by_missing_user_throws_exception() {
+        assertThatThrownBy(() -> emailVerificationService.resendVerificationToken(UUID.randomUUID()))
+                .isInstanceOf(InvalidTokenException.class);
+    }
+
+    @Test
+    void when_resend_invoked_by_user_with_verified_email_throws_exception() {
+        User user = generatePersistedUser();
+        user.setEmailVerified(true);
+        userRepository.save(user);
+
+        assertThatThrownBy(() -> emailVerificationService.resendVerificationToken(user.getPublicId()))
+                .isInstanceOf(InvalidTokenException.class);
+    }
+
+    @Test
+    void when_resend_invoked_email_verification_event_published() {
+        User user = generatePersistedUser();
+
+        emailVerificationService.resendVerificationToken(user.getPublicId());
+
+        assertThat(applicationEvents.stream(EmailVerificationRequestedEvent.class))
+                .singleElement()
+                .extracting(EmailVerificationRequestedEvent::publicId)
+                .isEqualTo(user.getPublicId());
+    }
+
     private Consumer<String, EmailVerificationRequestedEvent> createTestConsumer() {
         var props = KafkaTestUtils.consumerProps(embeddedKafkaBroker, "registration-test", true);
 
@@ -131,16 +177,36 @@ public class EmailVerificationIntegrationTest extends AbstractIntegrationTest {
                 .createConsumer();
     }
 
-
     private String issueVerificationToken(User user) {
         var generatedToken = emailVerificationTokenService.generate();
-        emailVerificationRepository.save(EmailVerification.builder()
-                .user(user)
-                .tokenHash(generatedToken.tokenHash())
-                .expiresAt(Instant.now().plus(emailVerificationTokenProperties.expiry()))
-                .build());
+        generatePersistedEmailVerification(user, generatedToken.tokenHash());
 
         return generatedToken.rawToken();
+    }
+
+    private VerificationWrapper issueVerificationTokenAndGetVerification(User user) {
+        var generatedToken = emailVerificationTokenService.generate();
+        var rawToken = generatedToken.rawToken();
+        return new VerificationWrapper(rawToken, generatePersistedEmailVerification(user, generatedToken.tokenHash()));
+    }
+
+    private @NonNull EmailVerification generatePersistedEmailVerification(User user, String tokenHash) {
+        return emailVerificationRepository.save(EmailVerification.builder()
+                .user(user)
+                .tokenHash(tokenHash)
+                .expiresAt(Instant.now().plus(emailVerificationTokenProperties.expiry()))
+                .build());
+    }
+
+    private @NonNull User generatePersistedUser() {
+        return userRepository.saveAndFlush(User.builder()
+                .username("johndoe")
+                .email("johndoe@example.com")
+                .emailVerified(false)
+                .build());
+    }
+
+    private record VerificationWrapper(String rawToken, EmailVerification emailVerification) {
     }
 
 }
